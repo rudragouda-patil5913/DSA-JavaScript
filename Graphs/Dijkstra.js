8cc97a814e4a498f794c302e7d0399ded350e247
Rudragouda Patil <rudragouda.spatil1298@gmail.com>
2024-12-12 12:49:22 +0530

    Dijkstra algorithm

--- Graphs/Dijkstra.js
+++ Graphs/Dijkstra.js
@@ -16,26 +16,23 @@ class MinPriorityQueue {
 }
 
 class Solution {
-  dijskstra(adj, src) {
+  // Function to find the shortest distance of all the vertices
+  // from the source vertex src.
+  dijkstra(adj, src) {
+    // code here
     const distTo = new Array(adj.length).fill(Infinity);
     const pq = new MinPriorityQueue();
     distTo[src] = 0;
     pq.enqueue(src, 0);
     while (!pq.isEmpty()) {
-      const { node, dist } = pq.dequeue();
+      const { node, priority } = pq.dequeue();
       for (let [u, v] of adj[node]) {
-        if (dist + v < distTo[u]) {
-          distTo[u] = dist + v;
-          pq.enqueue(u, dist + v);
+        if (priority + v < distTo[u]) {
+          distTo[u] = priority + v;
+          pq.enqueue(u, priority + v);
         }
       }
     }
     return distTo;
   }
 }
-
-
-
-
-
-
